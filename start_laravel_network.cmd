e0cac21902055e79165e750b186e692c3648ec38
nashwa-04 <nash239204@gmail.com>
2026-01-27 16:09:50 +0300

    Update log file and network startup script

--- start_laravel_network.cmd
+++ start_laravel_network.cmd
@@ -1,6 +1,6 @@
 @echo off
 REM Start Laravel server on network IP
-echo Starting Laravel server on network IP: 192.168.43.19:8000
+echo Starting Laravel server on network IP: 10.28.57.151:8000
 echo Other devices on the local network can access this server
 echo.
 cd backend
